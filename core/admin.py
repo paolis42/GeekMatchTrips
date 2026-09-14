7856e61f69c07c89e8cbda2af9e0a7296c6f6053
Paola Torrent <paorovirato@gmail.com>
2026-09-14 12:01:19 -0300

    Configurar administrador de Django para Registro

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Registro
 
-# Register your models here.
+
+@admin.register(Registro)
+class RegistroAdmin(admin.ModelAdmin):
+    list_display = (
+        "nombre_lugar",
+        "nivel_interes",
+        "precio",
+        "presupuesto",
+        "resultado",
+        "fecha",
+    )
+
+    list_filter = (
+        "resultado",
+        "eliminado",
+    )
+
+    search_fields = (
+        "nombre_lugar",
+    )
+
+    readonly_fields = (
+        "fecha",
+        "fecha_eliminacion",
+    )
+
